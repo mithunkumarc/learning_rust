d47596c6b7e603a2ad66c9c2b9c662514ac19bba
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2022-02-06 20:43:24 +0530

    Update type_check.rs

--- rust_programming_ref_guide/type_check.rs
+++ rust_programming_ref_guide/type_check.rs
@@ -6,3 +6,16 @@ fn main() {
     let s = "hello";
     type_of(&s); // output : &str , pass by reference
 }
+
+
+/* pass by value works too: but if the object is big(many states) copying takes memory
+fn type_of<T>(_: T) { // confusing, in c we use *T, but rust uses &
+    println!("{}", std::any::type_name::<T>());
+}
+
+fn main() {
+    let s = "hello";
+    type_of(s); // &str , pass by reference
+}
+
+*/
