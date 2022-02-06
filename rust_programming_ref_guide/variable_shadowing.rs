8baf141c6ba8dc4ea5ee53b452b2804af5fd2bf4
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2022-02-06 20:18:09 +0530

    Create variable_shadowing.rs

---
+++ rust_programming_ref_guide/variable_shadowing.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    // variable declared as int
+    let x = 100;
+    println!("the value of x is : {}",x); // int
+
+    // redeclaring x with string datatype, old variable is lost / destroyed
+    let x = "hello";
+    println!("the value of x is : {}",x); // string
+}
